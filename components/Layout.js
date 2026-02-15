bd73d18b8c203cc19f0c05c9c47938f861baeff5
Rehan Suman <rehansuman41008@gmail.com>
2026-02-15 18:56:04 +0530

    chore: update branding to KIITFEST 9.0

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import ScrollToTopButton from './ScrollToTopButton';
 
-export default function Layout({ children, title = 'TANV', variant = 'dark', hideFooter = false }) {
+export default function Layout({ children, title = 'KIITFEST 9.0 – TANV WHY', variant = 'dark', hideFooter = false }) {
   return (
     <>
       <Head>
